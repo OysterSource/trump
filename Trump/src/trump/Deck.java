23b8b6f1307baaf52bfebaa72e8a1aa15066b276
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-02 20:45:45 +0900

    9/2

--- Trump/src/trump/Deck.java
+++ Trump/src/trump/Deck.java
@@ -3,21 +3,25 @@ package trump;
 public class Deck {
 	public Deck() {
 		System.out.println("デッキの初期化");
-		Card card3 = new Card("ダイヤ", 7);
+		Card card = new Card("ダイヤ", 7);
 	}
 
 	public void build() {
 		System.out.println("ビルド");
 
 	}
+	private Card c1;
 
-	public void add() {
-		System.out.println("アド");
+	public void add(Card card) {
+		System.out.println("add");
+		this.c1 = card;
+
 	}
 
-	public void draw() {
+	public Card draw() {
 		System.out.println("ドロー");
 
-	}
+		return this.c1;
 
+	}
 }
