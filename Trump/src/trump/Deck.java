d9467dac2accaa1b9c019737425808ff0d44ee5a
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-08 19:38:30 +0900

    9月8日カード全てをデッキに実装・シャッフル・ドロー機能追加

--- Trump/src/trump/Deck.java
+++ Trump/src/trump/Deck.java
@@ -15,47 +15,41 @@ public class Deck {
 	public void build() {
 		//52枚のカードアレイリストに入れる
 		System.out.println("ビルド" + "52枚のカードアレイリストに入れる");
-		Card card1 = new Card("スペード", 4);
-		this.add(card1);
-		System.out.println(cardList);
+		for(int i = 1; i < 14; i++) {
+			Card card1 = new Card("スペード", i);
+			Card card2 = new Card("ハート", i);
+			Card card3 = new Card("ダイヤ", i);
+			Card card4 = new Card("クラブ", i);
+			this.add(card1);
+			this.add(card2);
+			this.add(card3);
+			this.add(card4);
 
-		/* System.out.println("add");
-		 * 
-		 * cardList.add(card1);
-		 * 
-		 * cardList.add(card2);
-		 * 
-		 * cardList.add(card3);
-		 * 
-		 * cardList.add(card4); */
-		/* String[] array = { "スペード", "ハート", "ダイヤ", "クラブ" };
-		 * for(int i = 1; i < 14; i++) {
-		 * Card card1 = new Card("スペード", i);
-		 * Card card2 = new Card("ハート", i);
-		 * Card card3 = new Card("ダイヤ", i);
-		 * Card card4 = new Card("クラブ", i);
-		 * //faceメソッドをよびだしてコンソールに表示する
-		 * System.out.println(card1.face());
-		 * System.out.println(card2.face());
-		 * System.out.println(card3.face());
-		 * System.out.println(card4.face());
-		 * } */
-
+			System.out.println(card1.face());
+			System.out.println(card2.face());
+			System.out.println(card3.face());
+			System.out.println(card4.face());
+		}
 	}
 
 	public void shuffle() {
 		System.out.println("ｼｬｯﾌﾙ");
 		Collections.shuffle(cardList);
+		System.out.println(cardList);
+
 	}
 
 	public void add(Card card) {
-		System.out.println("add");
-		this.cardList.add(card);
+		//System.out.println("add");
+		cardList.add(card);
 	}
 
 	public Card draw() {
 		System.out.println("ドロー");
-		return cardList.get(0);
+		Card card = cardList.get(0);
+		cardList.remove(0);
+		return card;
+
 	}
 }
 
--- Trump/src/trump/Main.java
+++ Trump/src/trump/Main.java
@@ -8,5 +8,18 @@ public class Main {
 		Deck deck = new Deck();
 
 		deck.build();
+
+		Card card5 = deck.draw();
+		deck.shuffle();
+		Card card6 = deck.draw();
+		Card card7 = deck.draw();
+
+		System.out.println(card5.face());
+		System.out.println(card6.face());
+		System.out.println(card7.face());
+
+		deck.add(card5);
+		Card card8 = deck.draw();
+		System.out.println(card8.face());
 	}
 }
