cacc7fee36d54a33f18f990fd9f66ea63f76c944
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-08 10:32:36 +0900

    Revert "9月7日"
    This reverts commit fe25cc84ede801840fd0e0b4cbe8ed649b72b064.

--- Trump/src/trump/Deck.java
+++ Trump/src/trump/Deck.java
@@ -21,10 +21,13 @@ public class Deck {
 			Card card2 = new Card("ハート", i);
 			Card card3 = new Card("ダイヤ", i);
 			Card card4 = new Card("クラブ", i);
-			
+
 			Deck.add(card1);
+			Deck.add(card2);
+			Deck.add(card3);
+			Deck.add(card4);
 		}
-		System.out.println();
+		System.out.println(cardList);
 
 	}
 	/* String[] array =
@@ -55,12 +58,12 @@ public class Deck {
 		// ここにコードを挿入
 
 	}
- public void shuffle() {
-	  System.out.println("ｼｬｯﾌﾙ");
-	  Collections.shuffle(cardList);
-	  }
-	  
-	 
+
+	public void shuffle() {
+		System.out.println("ｼｬｯﾌﾙ");
+		Collections.shuffle(cardList);
+	}
+
 	/* public void add(Card card) {
 	 * System.out.println("add");
 	 * this.cardList.add(card);
